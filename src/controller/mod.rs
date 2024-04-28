63ed5a53dd09ba91be7405da6f638a812dc02609
Lance Hart <me@lancehart.ca>
2024-04-28 15:14:14 -0400

    Added resource to LogController

--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -7,6 +7,9 @@ pub use list_type::FilterListType;
 
 /// The central controller for log settings and output
 pub struct LogController {
+    /// The name of this log source
+    resource: Cow<'static, str>,
+
     /// The type of list for filtering scopes
     filter_list_type: FilterListType,
 
@@ -25,11 +28,12 @@ pub struct LogController {
 
 impl LogController {
     /// Creates a new [`LogController`]
-    pub fn new<S: Into<Cow<'static, str>>>(
+    pub fn new<S1: Into<Cow<'static, str>>, S2: Into<Cow<'static, str>>>(
+        resource: S1,
         min_level: LogLevel,
         max_level: Option<LogLevel>,
         filter_list_type: FilterListType,
-        filter_list: Vec<S>,
+        filter_list: Vec<S2>,
         outputs: Vec<Box<dyn LogOutput>>,
     ) -> std::io::Result<Arc<Self>> {
         if let Some(max_level) = max_level {
@@ -41,6 +45,7 @@ impl LogController {
         let writer = LogWriter::new(outputs)?;
 
         Ok(Arc::new(LogController {
+            resource: resource.into(),
             filter_list_type,
             filter_list,
             min_level,
