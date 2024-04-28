be1b8478c12408bd134e6644c7024b02a4a6b0b4
Lance Hart <shipsimfan@hotmail.com>
2024-04-28 14:27:40 -0400

    Added LogRecord members

--- src/record/metadata.rs
+++ src/record/metadata.rs
@@ -8,27 +8,27 @@ use crate::LogRecord;
 /// The metadata about a [`LogRecord`]
 pub struct LogRecordMetadata {
     /// The time when an event occurred
-    pub timestamp: SystemTime,
+    timestamp: SystemTime,
 
     /// Request trace ID
-    pub trace_id: [u8; 16],
+    trace_id: [u8; 16],
 
     /// Request span ID
-    pub span_id: [u8; 16],
+    span_id: [u8; 16],
 
     /// The severity
-    pub level: LogLevel,
+    level: LogLevel,
 
     /// The source of this log
-    pub resource: Cow<'static, [u8]>,
+    resource: Cow<'static, [u8]>,
 
     /// The scope that emitted this log
-    pub scope: Cow<'static, [u8]>,
+    scope: Cow<'static, [u8]>,
 }
 
 impl LogRecordMetadata {
     /// Creates a new [`LogRecordMetadata`]
-    pub(crate) fn new<S1: Into<Cow<'static, [u8]>>, S2: Into<Cow<'static, [u8]>>>(
+    pub(super) fn new<S1: Into<Cow<'static, [u8]>>, S2: Into<Cow<'static, [u8]>>>(
         level: LogLevel,
         resource: S1,
         scope: S2,
@@ -41,15 +41,5 @@ impl LogRecordMetadata {
             resource: resource.into(),
             scope: scope.into(),
         }
-    }
-
-    /// Sets the source of this log
-    pub fn set_resource<S: Into<Cow<'static, [u8]>>>(&mut self, resource: S) {
-        self.resource = resource.into();
-    }
-
-    /// Sets the scope of this log
-    pub fn set_scope<S: Into<Cow<'static, [u8]>>>(&mut self, scope: S) {
-        self.scope = scope.into();
     }
 }
--- src/record/mod.rs
+++ src/record/mod.rs
@@ -1,3 +1,5 @@
+use std::borrow::Cow;
+
 mod level;
 mod metadata;
 mod serialized;
@@ -8,4 +10,25 @@ pub use metadata::LogRecordMetadata;
 pub(crate) use serialized::SerializedLogRecord;
 
 /// A message to be logged
-pub struct LogRecord {}
+pub struct LogRecord<'a> {
+    /// The metadata about this record
+    metadata: LogRecordMetadata,
+
+    /// The body of the log record
+    body: &'a dyn std::fmt::Display,
+}
+
+impl<'a> LogRecord<'a> {
+    /// Creates a new [`LogRecord`]
+    pub(crate) fn new<S1: Into<Cow<'static, [u8]>>, S2: Into<Cow<'static, [u8]>>>(
+        level: LogLevel,
+        resource: S1,
+        scope: S2,
+        body: &'a dyn std::fmt::Display,
+    ) -> Self {
+        LogRecord {
+            metadata: LogRecordMetadata::new(level, resource, scope),
+            body,
+        }
+    }
+}
