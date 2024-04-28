e3488831c61d108340fb20d53e04f47ff8bc45a2
Lance Hart <shipsimfan@hotmail.com>
2024-04-28 13:29:03 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+//! A logging library
+
+#![deny(missing_docs)]
+#![deny(rustdoc::private_intra_doc_links)]
+#![deny(rustdoc::unescaped_backticks)]
+#![deny(rustdoc::redundant_explicit_links)]
+#![warn(rustdoc::broken_intra_doc_links)]
