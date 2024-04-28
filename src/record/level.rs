5917adffe51c4ecc394201500848b9993d9174c0
Lance Hart <shipsimfan@hotmail.com>
2024-04-28 14:07:52 -0400

    Implemented LogLevel

--- src/record/level.rs
+++ src/record/level.rs
@@ -3,4 +3,43 @@
 use crate::LogRecord;
 
 /// The severity of a [`LogRecord`]
-pub enum LogLevel {}
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum LogLevel {
+    /// A fine-grained debugging event
+    Trace,
+
+    /// A debugging event
+    Debug,
+
+    /// An informational event
+    Info,
+
+    /// A warning event
+    Warning,
+
+    /// An error event
+    Error,
+
+    /// A fatal error
+    Fatal,
+}
+
+impl LogLevel {
+    /// Gets a string representation of a [`LogLevel`]
+    pub const fn as_str(&self) -> &'static str {
+        match self {
+            LogLevel::Trace => "trace",
+            LogLevel::Debug => "debug",
+            LogLevel::Info => "info",
+            LogLevel::Warning => "warning",
+            LogLevel::Error => "error",
+            LogLevel::Fatal => "fatal",
+        }
+    }
+}
+
+impl std::fmt::Display for LogLevel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(self.as_str())
+    }
+}
