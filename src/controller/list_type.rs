cf88887588d66f6896b5fe3b0b90e6e17a4f7921
Lance Hart <shipsimfan@hotmail.com>
2024-05-02 09:52:11 -0400

    Added FromStr implementations to FilterListType and LogLevel

--- src/controller/list_type.rs
+++ src/controller/list_type.rs
@@ -1,4 +1,4 @@
-use std::borrow::Cow;
+use std::{borrow::Cow, str::FromStr};
 
 /// The type of list for filtering scopes
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -9,6 +9,8 @@ pub enum FilterListType {
     /// Logs all scopes except those in the list
     Blacklist,
 }
+
+pub struct UnknownFilterListType;
 
 impl FilterListType {
     /// Checks if `item` should be allowed based on the `filter_list` and this type
@@ -25,5 +27,58 @@ impl FilterListType {
             FilterListType::Blacklist => !contains,
             FilterListType::Whitelist => contains,
         }
+    }
+}
+
+impl FromStr for FilterListType {
+    type Err = UnknownFilterListType;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        const TYPES: &[(&str, FilterListType)] = &[
+            ("white", FilterListType::Whitelist),
+            ("whitelist", FilterListType::Whitelist),
+            ("black", FilterListType::Blacklist),
+            ("blacklist", FilterListType::Blacklist),
+        ];
+
+        fn case_insensitive_compare(a: &str, b: &str) -> bool {
+            if a.len() != b.len() {
+                return false;
+            }
+
+            let mut a = a.chars();
+            let mut b = b.chars();
+            while let Some(a) = a.next() {
+                let b = b.next().unwrap();
+
+                if a.to_ascii_lowercase() != b.to_ascii_lowercase() {
+                    return false;
+                }
+            }
+
+            true
+        }
+
+        for (type_str, r#type) in TYPES {
+            if case_insensitive_compare(s, type_str) {
+                return Ok(*r#type);
+            }
+        }
+
+        Err(UnknownFilterListType)
+    }
+}
+
+impl std::error::Error for UnknownFilterListType {}
+
+impl std::fmt::Display for UnknownFilterListType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("unknown filter list type")
+    }
+}
+
+impl std::fmt::Debug for UnknownFilterListType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Display::fmt(self, f)
     }
 }
--- src/record/level.rs
+++ src/record/level.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 // rustdoc imports
 #[allow(unused_imports)]
 use crate::LogRecord;
@@ -24,6 +26,8 @@ pub enum LogLevel {
     Fatal,
 }
 
+pub struct UnknownLogLevel;
+
 impl LogLevel {
     /// Gets a string representation of a [`LogLevel`]
     pub const fn as_str(&self) -> &'static str {
@@ -41,5 +45,63 @@ impl LogLevel {
 impl std::fmt::Display for LogLevel {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(self.as_str())
+    }
+}
+
+impl FromStr for LogLevel {
+    type Err = UnknownLogLevel;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        const LEVELS: &[(&str, LogLevel)] = &[
+            ("trace", LogLevel::Trace),
+            ("debug", LogLevel::Debug),
+            ("info", LogLevel::Info),
+            ("information", LogLevel::Info),
+            ("warn", LogLevel::Warning),
+            ("warning", LogLevel::Warning),
+            ("err", LogLevel::Error),
+            ("error", LogLevel::Error),
+            ("fatal", LogLevel::Fatal),
+        ];
+
+        fn case_insensitive_compare(a: &str, b: &str) -> bool {
+            if a.len() != b.len() {
+                return false;
+            }
+
+            let mut a = a.chars();
+            let mut b = b.chars();
+            while let Some(a) = a.next() {
+                let b = b.next().unwrap();
+
+                if a.to_ascii_lowercase() != b.to_ascii_lowercase() {
+                    return false;
+                }
+            }
+
+            true
+        }
+
+        for (level_str, level) in LEVELS {
+            if case_insensitive_compare(s, level_str) {
+                return Ok(*level);
+            }
+        }
+
+        Err(UnknownLogLevel)
+    }
+}
+
+impl std::error::Error for UnknownLogLevel {}
+
+impl std::fmt::Display for UnknownLogLevel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str("unknown log level")
+    }
+}
+
+impl std::fmt::Debug for UnknownLogLevel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Display::fmt(self, f)
     }
 }
